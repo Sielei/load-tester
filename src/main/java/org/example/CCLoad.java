c47731c00126d319b0ea03dbd5da96dc930da4ee
Sielei Herman <hsielei@gmail.com>
2024-01-08 21:12:21 +0300

    Set up all cli commands for the load tester using picocli

---
+++ src/main/java/org/example/CCLoad.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import picocli.CommandLine;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "ccload", description = "Load test an application", mixinStandardHelpOptions = true, version = "1.0", subcommands = {})
+public class CCLoad  implements Callable<Integer> {
+    @CommandLine.Option(names = {"-u", "--url"}, required = true, description = "URL of the application to load test")
+    private String url;
+    @CommandLine.Option(names = {"-n", "--number-of-requests"}, required = true, description = "Number of requests to send")
+    private int numberOfRequests;
+    @CommandLine.Option(names = {"-c", "--number-of-concurrent-requests"}, required = true, description = "Number of concurrent requests to send")
+    private int numberOfConcurrentRequests;
+    @CommandLine.Option(names = {"-f", "--file-name"}, required = true, description = "Name of the file containing urls to load test")
+    private String fileName;
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new CCLoad()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Loading test application...");
+        System.out.println("URL: " + url);
+        System.out.println("Number of requests: " + numberOfRequests);
+        System.out.println("Number of concurrent requests: " + numberOfConcurrentRequests);
+        System.out.println("File name: " + fileName);
+        System.out.println("Loading test application complete.");
+        System.out.println("Results will be displayed shortly.");
+        System.out.println("Thank you for using org.example.CCLoad.");
+        System.out.println("Goodbye.");
+        System.exit(0); // Exit the program after displaying the results.
+        return 0; // Return 0 to indicate success. This is not necessary, but it is a good practice.
+    }
+}
